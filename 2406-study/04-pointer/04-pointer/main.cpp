d83d75fd74464be01d558e214dae8921b130b3da
kyuriiii <kyuri99@naver.com>
2024-06-13 22:04:11 +0900

    포인터 개념 공부

---
+++ 2406-study/04-pointer/04-pointer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  04-pointer
+//
+//  Created by 김규리 on 6/13/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
